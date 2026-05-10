bb2e0f5c463b51eca7e3b5de657a71f579ac47ec
s1698 <25021698@vnu.edu.vn>
2026-05-10 23:59:45 +0700

    made Response Serializable

--- shared/src/main/java/com/app/shared/network/Response.java
+++ shared/src/main/java/com/app/shared/network/Response.java
@@ -1,8 +1,9 @@
 package com.app.shared.network;
 
 import java.io.Serial;
+import java.io.Serializable;
 
-public record Response(boolean success, String message, Object payload) {
+public record Response(boolean success, String message, Object payload) implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 }
