c99e5e882b3bae7789e529ca2661e3c13635cbcc
s1698 <25021698@vnu.edu.vn>
2026-05-27 22:47:07 +0700

    introduce reserved balance để làm auto bid

--- shared/src/main/java/com/app/shared/model/user/Bidder.java
+++ shared/src/main/java/com/app/shared/model/user/Bidder.java
@@ -2,17 +2,22 @@ package com.app.shared.model.user;
 
 public class Bidder extends User {
     private double balance;
+    private double reservedBalance;
 
     @Override
     public boolean canBid() { return true; }
 
-    public Bidder(String username, String password, String email, double initialBalance) {
+    public Bidder(String username, String password, String email, double initialBalance, double reservedBalance) {
         super(username, password, email, "BIDDER");
         this.balance = initialBalance;
+        this.reservedBalance = reservedBalance;
     }
-
 
     public double getBalance() { return balance; }
     public void setBalance(double balance) { this.balance = balance; }
+
+    public double getReservedBalance() { return reservedBalance; }
+    public void setReservedBalance(double reservedBalance) { this.reservedBalance = reservedBalance; }
+}
 }
 
